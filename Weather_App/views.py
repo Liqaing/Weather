0c39cd4002d9f311a3d33b2e7683fdca5df7f9f3
Liqaing <huotrithy01@gmail.com>
2023-05-18 21:38:24 +0700

    Set up api and make api call successful

--- Weather_App/views.py
+++ Weather_App/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+import requests
 
+from django.shortcuts import render
 from django.http import HttpResponse
+
+# URL and API key for making API call
+API_KEY = "4da3135aeca586f32bb18c5c27d81cdb"
+Geocoding_API_URL = "http://api.openweathermap.org/geo/1.0/direct?q={}&limit={}&appid={}"
+Weather_API_URl = "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}"
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+
+    # Get city coordinate
+    lat, lon = get_geocoding_api_response("London", "1")
+
+    # Get weather for that city
+    get_weather_api_response(lat, lon)
+
+    return HttpResponse()
+
+
+# Function to send api call for Geocoding API
+def get_geocoding_api_response(city: str, limit: str):
+    
+    # Send Get request to Geocoding url and format response as a json object
+    geocoding_api_respone = requests.get(Geocoding_API_URL.format(city, limit, API_KEY)).json()
+    
+    # Get latitude and longitude of the city location 
+    lat, lon = geocoding_api_respone[0]["lat"], geocoding_api_respone[0]["lon"]
+
+    return lat, lon
+
+def get_weather_api_response(lat: int, lon: int):
+
+    # Send the request
+    weather_api_response = requests.get(Weather_API_URl.format(lat, lon, API_KEY)).json()
+
+    print(weather_api_response)
+
+    return
+
