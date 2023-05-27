8368ec3871a983951d9810da235b8dee89cb116c
Huot Rithy <huotrithy01@gmail.com>
2023-05-27 17:19:31 +0700

    save

--- Weather/settings.py
+++ Weather/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k6+t+vgj79nwj@n+28jk(29x_d0nea1t$-epb^-%yt-wu2ds#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
