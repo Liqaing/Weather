d0ceffdd2649c8afde6cfccf84f7dcd8d33bb1ec
Liqaing <huotrithy01@gmail.com>
2023-06-24 20:43:37 +0700

    Set up for pythonanywhere

--- Weather/settings.py
+++ Weather/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -23,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k6+t+vgj79nwj@n+28jk(29x_d0nea1t$-epb^-%yt-wu2ds#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
