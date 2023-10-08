d4fc474cbc0175e379a9924baf1fa6be5ff92038
Rithy <huotrithy01@gmail.com>
2023-10-08 17:19:06 +0700

    Collect static files to single folder for hosting

--- Weather/settings.py
+++ Weather/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from os import path
 
 from pathlib import Path
 
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
